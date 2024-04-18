a3da5edc2456e20e513a3b8444506df8c22cf4a3
Irina Ichim <onlyirina7@gmail.com>
2024-04-18 03:15:18 +0200

    creando Navbar basico, carpeta components y pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom'; // Elimina la importación de Switch aquí
+import '@fortawesome/fontawesome-free/css/all.css';
 
-function App() {
+
+import Navbar from './components/Navbar'; 
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+    
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; // Importa Link para manejar las rutas
+
+
+const Navbar = () => {
+  const navbarStyles = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '10px 20px',
+    backgroundColor: '#333',
+    color: '#fff',
+  };
+
+  const logoStyles = {
+    width: '120px',
+  };
+
+  const navLinksStyles = {
+    listStyleType: 'none',
+    display: 'flex',
+    gap: '20px',
+  };
+
+  const iconStyles = {
+    marginRight: '10px',
+  };
+
+  return (
+    <nav style={navbarStyles}>
+      {/* Logo */}
+      <div className="logo">
+        <Link to="/">
+          <img src="../logo.gif" alt="BellezaLux Logo" style={logoStyles} />
+        </Link>
+      </div>
+      
+      {/* Enlaces */}
+      <ul className="nav-links" style={navLinksStyles}>
+        <li><Link to="/" style={{ color: '#fff', textDecoration: 'none' }}>Home</Link></li>
+        <li><Link to="/marcas" style={{ color: '#fff', textDecoration: 'none' }}>Marcas de Lujo</Link></li>
+        <li><Link to="/cestas" style={{ color: '#fff', textDecoration: 'none' }}>Cestas de Regalo</Link></li>
+        <li><Link to="/blog" style={{ color: '#fff', textDecoration: 'none' }}>Blog</Link></li>
+      </ul>
+
+      {/* Iconos */}
+      <div className="icons" style={{ display: 'flex', gap: '10px' }}>
+        <Link to="/login" style={iconStyles}>
+          <i className="fa-regular fa-user"></i>
+        </Link>
+        <Link to="/carrito" style={iconStyles}>
+          <i className="fas fa-shopping-cart"></i>
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+
